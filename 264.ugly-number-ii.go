4263839606e797cf50211c1a74c3801613abb38a
mingor <mmmmingot@gmail.com>
2021-04-24 23:28:12 +0800

    finish: 264

---
+++ 264.ugly-number-ii.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=264 lang=golang
+ *
+ * [264] Ugly Number II
+ */
+
+// @lc code=start
+func nthUglyNumber(n int) int {
+	dp, p2, p3, p5 := make([]int, n+1), 1, 1, 1
+	dp[0], dp[1] = 0, 1
+	for i := 2; i <= n; i++ {
+		x2, x3, x5 := dp[p2]*2, dp[p3]*3, dp[p5]*5
+		dp[i] = min(min(x2, x3), x5)
+		if dp[i] == x2 {
+			p2++
+		}
+		if dp[i] == x3 {
+			p3++
+		}
+		if dp[i] == x5 {
+			p5++
+		}
+	}
+	return dp[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+// @lc code=end
