bdfbd35e26c75a68a06330e7ec9efdbfe0e683da
mingorxie <mingor@mingorxiedeMacBook-Pro.local>
2022-09-01 21:43:17 +0800

    solve 328

---
+++ 328.odd-even-linked-list.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=328 lang=cpp
+ *
+ * [328] Odd Even Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* oddEvenList(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) return head;
+
+    ListNode* odd = head;
+    ListNode* even = head->next;
+    ListNode* dummy_even = even;
+
+    while (odd && even && even->next) {
+      odd->next = odd->next->next;
+      even->next = even->next->next;
+
+      odd = odd->next;
+      even = even->next;
+    }
+
+    odd->next = dummy_even;
+
+    return head;
+  }
+};
+// @lc code=end
+
