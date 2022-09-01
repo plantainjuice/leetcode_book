d2dddb3169813318897c86321332a07fce015c33
mingorxie <mingor@mingorxiedeMacBook-Pro.local>
2022-09-01 20:16:21 +0800

    solve 319

---
+++ 319.bulb-switcher.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=319 lang=cpp
+ *
+ * [319] Bulb Switcher
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int bulbSwitch(int n) {
+      return sqrt(n);
+    }
+};
+// @lc code=end
+
