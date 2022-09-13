ecbf37cd79faf483e518df9a0b5de23f641488d8
mingorxie <mingor@mingorxiedeMacBook-Pro.local>
2022-09-13 11:30:44 +0800

    solve 96:sparkles:

---
+++ 96.unique-binary-search-trees.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=96 lang=cpp
+ *
+ * [96] Unique Binary Search Trees
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int numTrees(int n) {
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        dp[i] += dp[j - 1] * dp[i - j];
+      }
+    }
+
+    return dp[n];
+  }
+};
+// @lc code=end
+
