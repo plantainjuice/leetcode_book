8dcf8fc598eb2b7aa6811e64e6e39347a7db54d3
mingorxie <mingor@mingorxiedeMacBook-Pro.local>
2022-09-30 15:33:35 +0800

    solve 1128

--- 1128.number-of-equivalent-domino-pairs.cpp
+++ 1128.number-of-equivalent-domino-pairs.cpp
@@ -6,10 +6,32 @@
 
 // @lc code=start
 class Solution {
-public:
-    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        
+ public:
+  int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+    unordered_map<int, int> mp;
+
+    for (vector<int>& v : dominoes) {
+      int id = 0;
+      if (v[0] > v[1])
+        id = (v[1] << 16) + v[0];
+      else
+        id = (v[0] << 16) + v[1];
+
+      mp[id]++;
     }
+
+    int ans = 0;
+
+    for (auto& kv : mp) {
+      int cnt = kv.second;
+      if (cnt == 1) continue;
+
+      // Cn-2
+      ans += cnt * (cnt - 1) / 2;
+    }
+
+    return ans;
+  }
 };
 // @lc code=end
 
