763d8c82356731184b18f12048adef2b07253948
mingorxie <mingor@mingorxiedeMacBook-Pro.local>
2022-09-30 14:08:17 +0800

    optimize 73

--- 73.set-matrix-zeroes.cpp
+++ 73.set-matrix-zeroes.cpp
@@ -6,27 +6,33 @@
 
 // @lc code=start
 class Solution {
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+ public:
+  void setZeroes(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
 
-        vector<bool> rows(m, false);
-        vector<bool> cols(n, false);
+    bool firstRow = false;
+    bool firstCol = false;
 
-        for(int i = 0; i < m; ++i) {
-          for (int j = 0; j < n; ++j) {
-            rows[i] = rows[i] || (matrix[i][j] == 0);
-            cols[j] = cols[j] || (matrix[i][j] == 0);
-          }
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (matrix[i][j] == 0) {
+          if(i == 0) firstRow = true;
+          if(j == 0) firstCol = true;
+
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
         }
-
-        for(int i = 0; i < m; ++i) {
-          for (int j = 0; j < n; ++j) {
-            if (rows[i] || cols[j]) matrix[i][j] = 0;
-          }
-        }
+      }
     }
+
+    for (int i = 1; i < m; ++i)
+      for (int j = 1; j < n; ++j)
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+
+    if(firstRow) for(int j = 0; j < n; ++j) matrix[0][j] = 0;
+    if(firstCol) for(int i = 0; i < m; ++i) matrix[i][0] = 0;
+  }
 };
 // @lc code=end
 
