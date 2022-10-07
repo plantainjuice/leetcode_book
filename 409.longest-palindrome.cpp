8fbc6cbf2a38c6728138ccb64c4871729023d88d
mingor <mmmmingor@gmail.com>
2022-10-07 12:00:45 +0800

    solve 409

---
+++ 409.longest-palindrome.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=409 lang=cpp
+ *
+ * [409] Longest Palindrome
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int longestPalindrome(string s) {
+    int freq[128] = {};
+    for (auto c : s) ++freq[c];
+    int OddGroup = 0;  // count of uniq char which shows up odd times, help me
+                       // if you have better naming
+    for (auto i : freq) OddGroup += i & 1;
+    return s.size() - OddGroup + (OddGroup > 0);
+  }
+};
+// @lc code=end
+
