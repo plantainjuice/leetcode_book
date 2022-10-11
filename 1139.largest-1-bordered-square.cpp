87b264231bfc5401256ce38ddccd4ed470e7f2af
mingorxie <mmmmingor@gmail.com>
2022-10-11 14:35:49 +0800

    solve 1139

---
+++ 1139.largest-1-bordered-square.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1139 lang=cpp
+ *
+ * [1139] Largest 1-Bordered Square
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int largest1BorderedSquare(vector<vector<int>>& grid) {
+        
+    }
+};
+// @lc code=end
+
