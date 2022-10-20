7b4a854f1ef0af5b23f11d757835319bb54cc23b
mingorxie <mmmmingor@gmail.com>
2022-10-20 15:57:52 +0800

    solve 779:ambulance:

---
+++ 779.k-th-symbol-in-grammar.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=779 lang=cpp
+ *
+ * [779] K-th Symbol in Grammar
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int kthGrammar(int N, int K) {
+    if (N == 1) return 0;
+
+	if (K % 2 == 0) return (kthGrammar(N - 1, K / 2) == 0) ? 1 : 0;
+	else return (kthGrammar(N - 1, (K + 1) / 2) == 0) ? 0 : 1;
+  }
+};
+// @lc code=end
+
