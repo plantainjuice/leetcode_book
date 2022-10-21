f3d028f8f66fe01b0d8ba1d3fdb8d4fa1e00d31d
mingorxie <mmmmingor@gmail.com>
2022-10-21 11:03:55 +0800

    solve 219:sparkles:

---
+++ 219.contains-duplicate-ii.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=219 lang=cpp
+ *
+ * [219] Contains Duplicate II
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    unordered_map<int, int> m;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      if (m.find(nums[i]) != m.end()) {
+        if (i - m[nums[i]] <= k) return true;
+      }
+      m[nums[i]] = i;
+    }
+
+    return false;
+  }
+};
+// @lc code=end
+
